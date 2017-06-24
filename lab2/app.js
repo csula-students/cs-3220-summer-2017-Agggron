c42bdccdd35c8f3a32074234e551a6a7e5361876
Aaron Tang <aaronwt@berkeley.edu>
2017-06-24 13:38:19 -0700

    completed lab2, with functionality of sorting by ascending/descending name/quantity

--- lab2/app.js
+++ lab2/app.js
@@ -274,7 +274,6 @@ class StatusTable {
     sort (columnName) {
         // after sorting the array of statuses, re render item to update view
         if (columnName === "name_ascending") {
-            //debugger;
             var final_sorted_queue = [];
             for (var i = 0; i < this.store.queue.length; i++) {
                 var current_item = this.store.queue[i];
@@ -309,7 +308,6 @@ class StatusTable {
             this.store.queue = final_sorted_queue;
         }
         if (columnName === "name_descending") {
-            //debugger;
             var final_sorted_queue = [];
             for (var i = 0; i < this.store.queue.length; i++) {
                 var current_item = this.store.queue[i];
@@ -327,6 +325,75 @@ class StatusTable {
                         }
 
                         if (current_item[0] > final_sorted_queue[j][0]) {
+                            newly_sorted_queue.push(current_item);
+                            newly_sorted_queue.push(final_sorted_queue[j]);
+                            item_in_sort_position = true;
+                        } else {
+                            newly_sorted_queue.push(final_sorted_queue[j]);
+                            // if the last item was compared, and current_item is actually supposed to be the last one
+                            if (j == (final_sorted_queue.length - 1)) {
+                                newly_sorted_queue.push(current_item);
+                            }
+                        }
+                    }
+                    final_sorted_queue = newly_sorted_queue; 
+                }
+            }
+            this.store.queue = final_sorted_queue;
+        }
+
+        if (columnName === "quantity_ascending") {
+            var final_sorted_queue = [];
+            for (var i = 0; i < this.store.queue.length; i++) {
+                var current_item = this.store.queue[i];
+                if (i == 0) {
+                    final_sorted_queue.push(current_item);
+                } else {
+                    // second item must compare to all items in new queue
+                    var newly_sorted_queue = [];
+                    var item_in_sort_position = false;
+                    for (var j = 0; j < final_sorted_queue.length; j++) {
+                        // item in appropriate position, all remaining items in the final_sorted_queue can go to the end
+                        if (item_in_sort_position) {
+                            newly_sorted_queue.push(final_sorted_queue[j]);
+                            continue;
+                        }
+
+                        if (current_item[2] < final_sorted_queue[j][2]) {
+                            newly_sorted_queue.push(current_item);
+                            newly_sorted_queue.push(final_sorted_queue[j]);
+                            item_in_sort_position = true;
+                        } else {
+                            newly_sorted_queue.push(final_sorted_queue[j]);
+                            // if the last item was compared, and current_item is actually supposed to be the last one
+                            if (j == (final_sorted_queue.length - 1)) {
+                                newly_sorted_queue.push(current_item);
+                            }
+                        }
+                    }
+                    final_sorted_queue = newly_sorted_queue; 
+                }
+            }
+            this.store.queue = final_sorted_queue;
+        }
+        if (columnName === "quantity_descending") {
+            var final_sorted_queue = [];
+            for (var i = 0; i < this.store.queue.length; i++) {
+                var current_item = this.store.queue[i];
+                if (i == 0) {
+                    final_sorted_queue.push(current_item);
+                } else {
+                    // second item must compare to all items in new queue
+                    var newly_sorted_queue = [];
+                    var item_in_sort_position = false;
+                    for (var j = 0; j < final_sorted_queue.length; j++) {
+                        // item in appropriate position, all remaining items in the final_sorted_queue can go to the end
+                        if (item_in_sort_position) {
+                            newly_sorted_queue.push(final_sorted_queue[j]);
+                            continue;
+                        }
+
+                        if (current_item[2] > final_sorted_queue[j][2]) {
                             newly_sorted_queue.push(current_item);
                             newly_sorted_queue.push(final_sorted_queue[j]);
                             item_in_sort_position = true;
@@ -409,6 +476,12 @@ class StatusTable {
                     <br>
                     <br>
                     <button class="sort_descending_button">Sort By Descending Name!</button>
+                    <br>
+                    <br>
+                    <button class="sort_ascending_quantity_button">Sort By Ascending Quantity!</button>
+                    <br>
+                    <br>
+                    <button class="sort_descending_quantity_button">Sort By Descending Quantity!</button>
                 </td>
             </tr>`;
 
@@ -423,6 +496,14 @@ class StatusTable {
         let sortDescendingButton = document.querySelector('.sort_descending_button');
         sortDescendingButton.addEventListener('click', () => {
             this.sort("name_descending");
+        });
+        let sortAscendingQuantityButton = document.querySelector('.sort_ascending_quantity_button');
+        sortAscendingQuantityButton.addEventListener('click', () => {
+            this.sort("quantity_ascending");
+        });
+        let sortDescendingQuantityButton = document.querySelector('.sort_descending_quantity_button');
+        sortDescendingQuantityButton.addEventListener('click', () => {
+            this.sort("quantity_descending");
         });
     }
 }
